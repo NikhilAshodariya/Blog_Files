498ac1f6bd68e65accf6b0f63b5ce71d02b35584
Nikhil Ashodariya <nikhilashodariya123@gmail.com>
2017-12-23 10:18:37 +0530

    Create ObjectGraph.js

---
+++ ObjectGraph.js
@@ -0,0 +1,22 @@
+var temp = 5;
+function deposit(){
+  console.log("this is deposit");
+}
+function withdraw(){
+  console.log("this is withdraw");
+}
+function statement(){
+  console.log("This is statement");
+}
+
+function piggyBank(){
+  var obj = {
+    balance:0,
+    deposit:deposit,
+    withdraw:withdraw,
+    statement:statement
+  };
+  return obj;
+}
+var g = piggyBank();
+g.deposit();
